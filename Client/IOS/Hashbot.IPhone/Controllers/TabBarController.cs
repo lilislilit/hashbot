ce3b24d8af14d9f4e71e8bfae205445678fb562a
Elena Savelyeva <elana.savelyeva@touchin.ru>
2013-05-21 15:52:40 +0400

    Переминования табов

--- Client/IOS/Hashbot.IPhone/Controllers/TabBarController.cs
+++ Client/IOS/Hashbot.IPhone/Controllers/TabBarController.cs
@@ -14,25 +14,25 @@ namespace Hashbot.IPhone
 		{
 			_twitterNavController= new UINavigationController();
 			_twitterTab = new HashTagController();
-			_twitterTab.Title = _twitterTab.HashTag = "Twitter";
+			_twitterTab.Title = _twitterTab.HashTag = "#Twitter";
 			_twitterTab.TabBarItem.Image = UIImage.FromFile("ios/TabBar/icon_twitter.png");
 			_twitterNavController.PushViewController(_twitterTab, false);
 
 			_appleNavController = new UINavigationController();
 			_appleTab = new HashTagController();
-			_appleTab.Title = _appleTab.HashTag = "Apple";
+			_appleTab.Title = _appleTab.HashTag = "#Apple";
 			_appleTab.TabBarItem.Image = UIImage.FromFile("ios/TabBar/icon_apple.png");
 			_appleNavController.PushViewController(_appleTab, false);
 
 			_dribbleNavController = new UINavigationController();
 			_dribbleTab = new HashTagController();
-			_dribbleTab.Title = _dribbleTab.HashTag = "Dribble";
+			_dribbleTab.Title = _dribbleTab.HashTag = "#Dribble";
 			_dribbleTab.TabBarItem.Image = UIImage.FromFile("ios/TabBar/icon_dribbble.png");
 			_dribbleNavController.PushViewController(_dribbleTab, false);
 
 			_githubNavController = new UINavigationController();
 			_gitHubTab = new HashTagController();
-			_gitHubTab.Title = _gitHubTab.HashTag = "GitHub";
+			_gitHubTab.Title = _gitHubTab.HashTag = "#GitHub";
 			_gitHubTab.TabBarItem.Image = UIImage.FromFile("ios/TabBar/icon_github.png");
 		     _githubNavController.PushViewController(_gitHubTab, false);
 
