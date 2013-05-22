c198707b15ec8e04ec883c8bc42feff0b1bab617
Elena Savelyeva <elana.savelyeva@touchin.ru>
2013-05-22 19:56:13 +0400

    Лейаут твитта, сверен с версткой, переворачиваем

--- Client/IOS/Hashbot.IPhone/Controllers/TweetController.cs
+++ Client/IOS/Hashbot.IPhone/Controllers/TweetController.cs
@@ -19,15 +19,17 @@ namespace Hashbot.IPhone
 		private UIImageView _line;
 		private UIImageView _avatar;
 		private UIImageView _imageView;
-	
+		private UIFont _tweetFont;
+		private SizeF _twitLabelSize;
 
 		public TweetController(TwitterMessage tweet) : base ("TweetController", null)
 		{
 			_tweet = tweet;
+			_tweetFont = Fonts.Helvetica(15);
+			var tweetStringSize = (NSString)_tweet.Text;
+			_twitLabelSize = tweetStringSize.StringSize(_tweetFont, View.Bounds.Width - 60, UILineBreakMode.WordWrap);
 
 		}
-
-
 
 		public override void DidReceiveMemoryWarning()
 		{
@@ -39,61 +41,89 @@ namespace Hashbot.IPhone
 
 		public override void ViewDidLoad()
 		{ 
+			base.ViewDidLoad();
 			NavigationItem.Title = "Твит";
 			InitLayout();
-			base.ViewDidLoad();
+
 
 			// Perform any additional setup after loading the view, typically from a nib.
+
+		}
+
+		public override void DidRotate(UIInterfaceOrientation fromInterfaceOrientation)
+		{
+			base.DidRotate(fromInterfaceOrientation);
+
+			_tweetLabel.Frame = new RectangleF(20, _sourceLabel.Frame.Bottom + 2, View.Bounds.Width - 30, _twitLabelSize.Height);
+			_tweetLabel.SizeToFit();
+			_line.Frame = new RectangleF(20, _tweetLabel.Frame.Bottom + 2, View.Bounds.Width / 2, 1);
+			_dateLabel.Frame = new RectangleF(20, _line.Frame.Bottom + 2, _line.Bounds.Width / 3, 25);
+
+			var urlFont = Fonts.Helvetica(10);
+			var urlLabelWidth = _line.Frame.Width - _dateLabel.Frame.Width;
+			var urlStartPosition = _line.Frame.Right - urlLabelWidth;
+			_urlLabel.Frame = new RectangleF(urlStartPosition, _line.Frame.Bottom + 2, urlLabelWidth, 25);
+
 		}
 
 		void InitLayout()
 		{
 			_imageView = new UIImageView(UIImage.FromFile("ios/Tweets/bg.png"));
+			_imageView.AutoresizingMask = UIViewAutoresizing.All;
 			View.AddSubview(_imageView);
+
 			View.SendSubviewToBack(_imageView);
 			var tempAvatar = UIImage.FromFile(_tweet.TwitterUser.ImageUri);
 			var maskedAvatar = tempAvatar.GetMaskedAvatar(UIImage.FromFile("ios/Main/mask_avatar.png"));
 			_avatar = new UIImageView(maskedAvatar);
-			_avatar.Frame = new RectangleF(View.Bounds.X + 10, View.Bounds.X + 20, 64, 64);
+			_avatar.Frame = new RectangleF(20, 30, 64, 64);
 
-			var tweetFont = Fonts.Helvetica(18);
-			var tweetStringSize = (NSString)_tweet.Text;
-			var twitLabelSize = tweetStringSize.StringSize(tweetFont,View.Bounds.Width-60,UILineBreakMode.WordWrap);
-			_tweetLabel = new UILabel(new RectangleF(20,90, View.Bounds.Width - 60, twitLabelSize.Height));
-			_tweetLabel.BackgroundColor = UIColor.Clear;
-			_tweetLabel.Text = _tweet.Text;
-			_tweetLabel.Font = tweetFont;
-			_tweetLabel.TextColor = UIColor.FromRGB(65, 65, 65);
-			_tweetLabel.LineBreakMode = UILineBreakMode.WordWrap;
-			_tweetLabel.Lines = 0;
-			_tweetLabel.SizeToFit();
-		
-			var userFont = Fonts.HelveticaBold(25);
-			_userLabel = new UILabel(new RectangleF(80,20, View.Bounds.Width, 25));
+
+			var userFont = Fonts.HelveticaBold(22);
+			_userLabel = new UILabel(new RectangleF(_avatar.Frame.Right+20,50, View.Bounds.Width, 25));
 			_userLabel.BackgroundColor = UIColor.Clear;
 			_userLabel.Text = _tweet.TwitterUser.Name;
 			_userLabel.Font = userFont;
-			_userLabel.TextColor = UIColor.FromRGB(68, 100, 43);
+			_userLabel.TextColor = UIColor.FromRGB(42, 66, 114);
 
-			_line = new UIImageView(UIImage.FromFile("ios/Tweets/line.png"));
-			_line.Frame = new RectangleF(20, _tweetLabel.Frame.Bottom + 2, View.Bounds.Width - 60, 1);
 
-			var dateFont = Fonts.Helvetica(15);
-			_dateLabel = new UILabel(new RectangleF(20, _line.Frame.Bottom + 2,_line.Bounds.Width/3, 25));
-			_dateLabel.Text = _tweet.CreatedAt.ToString("dd:mm:yyyy");
-			_dateLabel.Font = dateFont;
-			_dateLabel.BackgroundColor = UIColor.Clear;
-			_dateLabel.TextColor = UIColor.FromRGB(119, 119, 119);
-
-			var sourceFont = Fonts.HelveticaBold(18);
-			_sourceLabel = new UILabel(new RectangleF(80,60, View.Bounds.Width, 25));
+			var sourceFont = Fonts.HelveticaBold(15);
+			_sourceLabel = new UILabel(new RectangleF(_avatar.Frame.Right+20,_userLabel.Frame.Bottom, View.Bounds.Width-_avatar.Frame.Width-40, 25));
 			_sourceLabel.BackgroundColor = UIColor.Clear;
 			_sourceLabel.Font = sourceFont;
 			_sourceLabel.TextColor = UIColor.FromRGB(65, 65, 65);
 			_sourceLabel.Text = _tweet.Source;
 
-			var urlFont = Fonts.Helvetica(15);
-			_urlLabel = new UILabel(new RectangleF(_dateLabel.Bounds.Right+15, _line.Frame.Bottom + 2,_line.Bounds.Width-_dateLabel.Bounds.Width, 25));
+
+			_tweetLabel = new UILabel(new RectangleF(20,_sourceLabel.Frame.Bottom, View.Bounds.Width - 30, _twitLabelSize.Height));
+			_tweetLabel.BackgroundColor = UIColor.Clear;
+			_tweetLabel.Text = _tweet.Text;
+			_tweetLabel.Font = _tweetFont;
+			_tweetLabel.TextColor = UIColor.FromRGB(65, 65, 65);
+			_tweetLabel.LineBreakMode = UILineBreakMode.WordWrap;
+			_tweetLabel.Lines = 0;
+			_tweetLabel.SizeToFit();
+
+
+
+
+			_line = new UIImageView(UIImage.FromFile("ios/Tweets/line.png"));
+			_line.Frame = new RectangleF(20, _tweetLabel.Frame.Bottom + 2, View.Bounds.Width / 2, 1);
+		
+			var dateFont = Fonts.Helvetica(10);
+			var dateString = (NSString)_tweet.CreatedAt.ToString("dd.MM.yyyy");
+			var dateStringSize = dateString.StringSize(dateFont);
+			_dateLabel = new UILabel(new RectangleF(20, _line.Frame.Bottom + 2,dateStringSize.Width, 25));
+			_dateLabel.Text = dateString;
+			_dateLabel.Font = dateFont;
+			_dateLabel.BackgroundColor = UIColor.Clear;
+			_dateLabel.TextColor = UIColor.FromRGB(119, 119, 119);
+
+	
+			var urlFont = Fonts.Helvetica(10);
+			var urlLabelWidth = _line.Frame.Width - _dateLabel.Frame.Width;
+			var urlStartPosition = _line.Frame.Right - urlLabelWidth + _dateLabel.Frame.Right;
+			_urlLabel = new UILabel(new RectangleF(urlStartPosition, _line.Frame.Bottom + 2,urlLabelWidth, 25));
 			_urlLabel.Text = _tweet.Url;
 			_urlLabel.Font = urlFont;
 			_urlLabel.BackgroundColor = UIColor.Clear;
