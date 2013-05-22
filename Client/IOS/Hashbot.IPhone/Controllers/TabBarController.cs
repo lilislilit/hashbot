222ccaf79843a79e51a4337887c38b04e5ca47bd
Elena Savelyeva <elana.savelyeva@touchin.ru>
2013-05-22 12:49:30 +0400

    Test2

--- Client/IOS/Hashbot.IPhone/Controllers/TabBarController.cs
+++ Client/IOS/Hashbot.IPhone/Controllers/TabBarController.cs
@@ -20,7 +20,7 @@ namespace Hashbot.IPhone
 
 			_appleNavController = new UINavigationController();
 			_appleTab = new HashTagController();
-			_appleTab.Title = _appleTab.HashTag = "#Apple";
+			_appleTab.Title = _appleTab.HashTag = "#Xamarin2";
 			_appleTab.TabBarItem.Image = UIImage.FromFile("ios/TabBar/icon_apple.png");
 			_appleNavController.PushViewController(_appleTab, false);
 
